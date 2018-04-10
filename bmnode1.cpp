1aacb758ceb112ce13b2f464b6355964ce1251df
Susheel <mlsusheel@gmail.com>
2018-04-10 16:13:59 +0800

    Fix the connection with Autoware  - able to run the entire length of autoware simulation  - free memory only on the local floats and not the CAN data structures

--- bmnode1.cpp
+++ bmnode1.cpp
@@ -13,11 +13,12 @@
 #include <sys/time.h>
 #include <stdio.h>
 #include <math.h>
+#include <exception>
 
 /* End BUSMASTER include header */
 /* Start BUSMASTER global variable */
 uint8_t i = 0;
-int j = 0;
+int k = 0;
 float latd,lond,h,lat,lad,lod,lon,m_lat,m_lon,m_h;
 time_t  t;
 /* End BUSMASTER global variable */
@@ -28,7 +29,9 @@ GCC_EXTERN void GCC_EXPORT OnTimer_timer_1000ms_1000( );
 //GCC_EXTERN void GCC_EXPORT OnMsg_All();
 
 GCC_EXTERN void GCC_EXPORT OnMsg_All(STCAN_MSG RxMsg);
-GCC_EXTERN void GCC_EXPORT OnMsgID_100(STCAN_MSG RxMsg);
+GCC_EXTERN void GCC_EXPORT OnMsgID_102(STCAN_MSG RxMsg);
+GCC_EXTERN void GCC_EXPORT OnError_Active(SCAN_ERR ErrorMsg);
+GCC_EXTERN void GCC_EXPORT OnError_Frame(SCAN_ERR ErrorMsg);
 /* End BUSMASTER Function Prototype  */
 
 /* Start BUSMASTER generated function - OnTimer_timer_100ms_100 */
@@ -64,45 +67,50 @@ double compute(float latd,float lond, float h)
   m_lat = (lad + lat / 60.0) * M_PI / 180;
   m_lon = (lod + lon / 60.0) * M_PI / 180;
   m_h = h;
-  Trace("m_lat = %f", m_lat);
-  Trace("m_lon = %f", m_lon);
-  Trace("m_h = %f", m_h);
+  Trace("m_lat = %f, m_lon= %f, m_h = %f", m_lat, m_lon,m_h);
   //Send result back to bm_server
   STCAN_MSG sMsgStruct;
   sMsgStruct.id = 0x101;
   sMsgStruct.dlc = 12;
   sMsgStruct.cluster = 1;
-  memcpy(&sMsgStruct.data, &m_lat, sizeof(float));
-  Trace("Sending m_lat to bm_server");
+  memcpy(sMsgStruct.data, &m_lat, sizeof(float));
+  Trace("2.1Sending m_lat");
   SendMsg(sMsgStruct);
-  memcpy(&sMsgStruct.data, &m_lon, sizeof(float));
-  Trace("Sending m_lon to bm_server");
+  memcpy(sMsgStruct.data, &m_lon, sizeof(float));
+  Trace("2.2Sending m_lon");
   SendMsg(sMsgStruct);
-  memcpy(&sMsgStruct.data, &m_h, sizeof(float));
-  Trace("Sending m_h to bm_server");
+  memcpy(sMsgStruct.data, &m_h, sizeof(float));
+  Trace("2.3Sending m_h");
   SendMsg(sMsgStruct);
 }
 /* Start BUSMASTER generated function - OnMsgID_100 */
-void OnMsgID_100(STCAN_MSG RxMsg)
+void OnMsgID_102(STCAN_MSG RxMsg)
 {
-	float rec_val = 0.0;
-	memcpy(&rec_val,RxMsg.data,sizeof(float));
-	Trace("2.Received message from bm_server - %f", rec_val);
-	if(j ==0){
-		latd = rec_val;
-		Trace("j = %d, Assigning latd = %f ",j,latd);
-		j++;
-	}
-	else if(j==1){
-		lond = rec_val;
-		Trace("j = %d, Assigning lond = %f ",j,lond);
-		j++;
-	}
-	else if(j==2){
-		h = rec_val;
-		Trace("j = %d, Assigning h = %f ",j,h);
-		j=0;
-		Trace("computing with latd,lond,h = %f %f %f ",latd,lond,h);
-		compute(latd,lond,h);
-	}
-}/* End BUSMASTER generated function - OnMsgID_100 */
+  k++;
+  float rec_val = 0.0;
+  memcpy(&rec_val, RxMsg.data, sizeof(float));
+  Trace("2.Received message - %f", rec_val);
+  if(k==1){
+    latd = rec_val;
+  }
+  else if(k==2){
+    lond = rec_val;  
+  }
+  else if(k==3){
+    h = rec_val;
+    k=0;
+    Trace("2.computing with latd,lond,h = %f %f %f ",latd,lond,h);
+    compute(latd,lond,h);
+  }
+ free(&rec_val);
+}
+/* End BUSMASTER generated function - OnMsgID_100 */
+
+/* Start BUSMASTER generated function - OnError_Active */
+
+/* Start BUSMASTER generated function - OnError_Frame */
+void OnError_Frame(SCAN_ERR ErrorMsg)
+{
+/* TODO */
+Trace("error frame");
+}/* End BUSMASTER generated function - OnError_Frame */
